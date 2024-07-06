fe32c5a29beff61fb69b78fbcbfe16d1e39f99e8
Samuel Darkhovsky <darkhovsky@usa.net>
2024-07-06 18:01:22 -0400

    LangChain requests with meta/llama3-70b-instruct LLM model started working

--- nvidia_contest_django_web_service/views.py
+++ nvidia_contest_django_web_service/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie, csrf_protect
 from .rag_langchain_nvidia_api import create_embeddings, create_chat_summarization_qa, create_chat_qa
-import datetime
 
 #@ensure_csrf_cookie
-#@csrf_exempt
+@csrf_exempt
+#@csrf_protect
 def process_user_message(request):
 
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    # request.body contains the image
+    # print("request.body: ", request.body)
 
     query = request.GET['query']
     print("query: ", query)
@@ -16,9 +16,11 @@ def process_user_message(request):
     qa = create_chat_qa()
     result = qa({"question": query})
     answer = result.get("answer")
+
     print("answer: ", answer)
 
     response = HttpResponse(answer)
     response.headers['Access-Control-Allow-Origin'] = "http://localhost:3000"
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
     return response
